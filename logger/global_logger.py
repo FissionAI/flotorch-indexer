f1cea63e23a95061eb1a8dbc73f20d71d8eda6e8
shivakrishnaah <shiva.krishnaah@gmail.com>
2025-02-12 22:16:46 +0530

    Updated the code to have only indexer logic

--- logger/global_logger.py
+++
@@ -1,9 +0,0 @@
-from logger.logger import Logger
-from logger.console_logger_provider import ConsoleLoggerProvider
-
-def get_logger():
-    """
-    Returns a singleton logger instance.
-    """
-    provider = ConsoleLoggerProvider()
-    return Logger(provider)
